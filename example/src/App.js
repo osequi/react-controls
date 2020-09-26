a3b7949bd41d7f4718b759def8ca45c91b114c3d
osequi <cs@osequi.com>
2020-09-26 12:41:03 +0300

    Ready to package

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,64 @@
 import React from "react";
+import PropTypes from "prop-types";
+import shortid from "shortid";
 
-const App = () => "App";
+/**
+ * Imports other components and hooks
+ */
+import Controls, { ControlsPropTypes } from "../../src/Controls";
+
+/**
+ * Defines the prop types
+ */
+const propTypes = {
+  controls: PropTypes.shape(ControlsPropTypes),
+};
+
+/**
+ * Defines the default props
+ */
+const defaultProps = {
+  controls: {
+    title: "Controls demo",
+    items: [
+      { id: shortid.generate(), type: "text", label: "Text", value: "text" },
+      {
+        id: shortid.generate(),
+        type: "checkbox",
+        label: "Checkbox Test",
+        value: true,
+      },
+      {
+        id: shortid.generate(),
+        type: "radio",
+        label: "Radio",
+        value: "Y",
+        items: [
+          {
+            id: shortid.generate(),
+            label: "X",
+            value: "X",
+          },
+          {
+            id: shortid.generate(),
+            label: "Y",
+            value: "Y",
+          },
+          {
+            id: shortid.generate(),
+            label: "Z",
+            value: "Z",
+          },
+        ],
+      },
+    ],
+  },
+};
+
+const App = (props) => {
+  const { controls } = props;
+
+  return <Controls {...controls} />;
+};
 
 export default App;
